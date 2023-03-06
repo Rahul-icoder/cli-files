05542d48378eea296db460ce506928e89719a744
Rahul-icoder <rahulkerma12345@gmail.com>
2023-03-06 19:15:57 +0530

    (response & ui) adding grid for responsive ui and also add bottom section for file properties and some styling of text

--- shared/ReadDir.go
+++ shared/ReadDir.go
@@ -4,8 +4,8 @@ import (
 	"io/fs"
 	"os"
 )
-func ReadDir(dirPath string)([]fs.DirEntry){
-	files, err := os.ReadDir(dirPath)
-	CheckError(err)
-	return files 
+
+func ReadDir(dirPath string) []fs.DirEntry {
+	files, _ := os.ReadDir(dirPath)
+	return files
 }
